652702b582cacda603b150768d415574b02c71a5
lideming <14901890+lideming@users.noreply.github.com>
2020-07-03 00:54:50 +0800

    add DB migration

---
+++ Migrations/20200702165226_track_version.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MCloudServer.Migrations
+{
+    public partial class track_version : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "version",
+                table: "tracks",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "version",
+                table: "tracks");
+        }
+    }
+}
