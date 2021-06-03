bfde49b8d1a31d8b4df2dc0ff4ba3058b39dae8a
lideming <14901890+lideming@users.noreply.github.com>
2021-06-03 23:23:40 +0800

    draft: FileRecord

---
+++ Models/FileRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCloudServer
+{
+    public class FileRecord
+    {
+        [Key]
+        public int id { get; set; }
+
+        public int size { get; set; }
+
+        public string path { get; set; }
+    }
+}
